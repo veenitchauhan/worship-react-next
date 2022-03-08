e024e86ad9190a458be8b2c9f99118bcacfbfd72
Veenit Chauhan <veenitchauhan@gmail.com>
2022-03-08 23:47:07 +0530

    inserted semicolon

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Head from 'next/head'
+import Head from 'next/head';
 
 import Link from 'next/link';
 import { getCategories } from '../services';
